1c78227edf4c902aaddb0383266777ab5112f2e5
Daniel Temple <DanielTempleUK@googlemail.com>
2013-11-30 09:02:47 +0000

    Adding solution to problem 6

---
+++ src/problem6/SumSquareDifferences.java
@@ -0,0 +1,31 @@
+package problem6;
+
+public class SumSquareDifferences {
+
+    public static void main(String[] args) {
+	
+	long sum = 0;
+	long squareSum = 0;
+	
+	for( int i = 1; i < 101; i++ ) {
+	    sum = sum + i;
+	    squareSum = squareSum + (i * i);
+	}
+	
+	System.out.println(sum);
+	System.out.println(squareSum);
+	
+	
+	long difference = (sum * sum) - squareSum;
+	
+	System.out.println(difference);
+	
+	
+	
+	
+	
+	
+    }
+    
+    
+}
