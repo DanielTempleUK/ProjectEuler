a15f367b51688f26512aa25680f10827812b635f
Daniel Temple <DanielTempleUK@GoogleMail.com>
2013-12-02 11:21:44 +0000

    Extended ignores and fixed problem 3 solution

--- src/problem3/HighestPrimeFactor.java
+++ src/problem3/HighestPrimeFactor.java
@@ -4,77 +4,48 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * This appears to not be my completed class.
- * Working on these prblems on more than one machine before I got github set up for them
- * problem wasn't the best plan....
- * 
- * @author Dan
- */
 public class HighestPrimeFactor {
 
-    private static final long INPUT = 600851475143L;
+	private static final double INPUT = 600851475143d;
 
-    private static final List<Long> primeFactors = new ArrayList<Long>();
+	private static final List<Double> primeFactors = new ArrayList<Double>();
 
-    public static void main(final String... args) {
-	HighestPrimeFactor.findPrimeFactor(HighestPrimeFactor.INPUT);
+	public static void main(final String... args) {
+		findPrimeFactors(INPUT);
 
-	Collections.sort(HighestPrimeFactor.primeFactors);
+		Collections.sort(primeFactors);
 
-	System.out.println(HighestPrimeFactor.primeFactors.get(HighestPrimeFactor.primeFactors.size()-1));
-    }
-
-    private static void findPrimeFactor(final long input) {
-	final long factor = HighestPrimeFactor.findFactorOf(input);
-	if(!HighestPrimeFactor.isPrime(factor)) {
-	    HighestPrimeFactor.findPrimeFactor(input/factor);
-	    HighestPrimeFactor.findPrimeFactor(factor);
-	}
-	else {
-	    HighestPrimeFactor.primeFactors.add(factor);
+		System.out.println(primeFactors.get(primeFactors.size()-1));
 	}
 
-    }
+	private static void findPrimeFactors(final double input) {
+		for ( double i = 2; i < Math.sqrt(input); i++ ) {
 
-    private static long findFactorOf(final double input) {
-	long a = (long) Math.ceil(Math.sqrt(HighestPrimeFactor.INPUT));
-	long b = a*a - HighestPrimeFactor.INPUT;
+			final double d = input / i;
 
-	while ( Math.sqrt(b) % 1 > 0  ) {
-	    a++;
-	    b = a*a - HighestPrimeFactor.INPUT;
+			if( ( d % 1 ) == 0 ) {
+				System.out.println(i + " is a factor");
+				if(isPrime(i)) {
+					primeFactors.add(i);
+				}
+			}
+		}
 	}
 
-	return (long) (a + Math.sqrt(b));
-    }
+	private static boolean isPrime(final double input) {
+		if( input == 2L || input == 3L || input == 5L || input == 7L ) {
+			return true;
+		}
+		if( input < 11) {
+			return false;
+		}
 
-    private static boolean isPrime(final long input) {
-	for ( int i = 2; i < Math.sqrt(input); i++ ) {
-	    if( input / i % 1 > 0 ) {
-		return false;
-	    }
+		for ( int i = 2; i <= Math.sqrt(input); i++ ) {
+			if( (input % i) == 0 ) {
+				return false;
+			}
+		}
+
+		return true;
 	}
-
-	return true;
-    }
-
-    /**
-     *     private static boolean isPrime(final long input) {
-	if( input == 2L || input == 3L || input == 5L || input == 7L ) {
-	    return true;
-	}
-	if( input < 11) {
-	    return false;
-	}
-
-	for ( int i = 2; i <= Math.sqrt(input); i++ ) {
-	    if( (input % i) == 0 ) {
-		return false;
-	    }
-	}
-
-	return true;
-    }
-     */
 }
