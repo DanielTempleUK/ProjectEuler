f2bd802744cdcbd899c766f596921438190bd293
Daniel Temple <DanielTempleUK@GoogleMail.com>
2013-12-02 11:11:41 +0000

    Not sure what changed, but commiting it as it still works

--- src/problem1/MultiplesOf3And5.java
+++ src/problem1/MultiplesOf3And5.java
@@ -4,15 +4,15 @@ import java.math.BigDecimal;
 
 public class MultiplesOf3And5 {
 
-    public static void main(final String... args) {
-	BigDecimal total = BigDecimal.ZERO;
+	public static void main(final String... args) {
+		BigDecimal total = BigDecimal.ZERO;
 
-	for ( int i = 0; i < 1000; i++) {
-	    if( i % 3 == 0 || i % 5 == 0 ) {
-		total = total.add(BigDecimal.valueOf(i));
-	    }
+		for ( int i = 0; i < 1000; i++) {
+			if( i % 3 == 0 || i % 5 == 0 ) {
+				total = total.add(BigDecimal.valueOf(i));
+			}
+		}
+
+		System.out.println(total);
 	}
-
-	System.out.println(total);
-    }
 }
