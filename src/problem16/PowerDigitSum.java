7b1e8272c8c919132ab76f1a33d416ab5327af9d
Daniel Temple <DanielTempleUK@googlemail.com>
2013-12-06 12:00:16 +0000

    Adding solution to problem 16

---
+++ src/problem16/PowerDigitSum.java
@@ -0,0 +1,24 @@
+package problem16;
+
+import java.math.BigDecimal;
+
+/**
+ * Solution to problem 16:
+ * 
+ * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * What is the sum of the digits of the number 2^1000?
+ */
+public class PowerDigitSum {
+
+    public static void main(final String[] args) {
+	final BigDecimal two = BigDecimal.valueOf(2);
+	final BigDecimal power = two.pow(1000);
+
+	final char[] digits = power.toString().toCharArray();
+	long total = 0;
+	for(int i=0; i<digits.length; i++) {
+	    total += Long.valueOf(""+digits[i]);
+	}
+	System.out.println(total);
+    }
+}
