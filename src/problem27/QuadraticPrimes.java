a6dc20721dfa014575a15da0d707d029a9045f34
Daniel Temple <DanielTempleUK@GoogleMail.com>
2013-12-11 08:34:58 +0000

    reformatting the comments in problem 27

--- src/problem27/QuadraticPrimes.java
+++ src/problem27/QuadraticPrimes.java
@@ -2,7 +2,6 @@ package problem27;
 
 import utilities.PrimalityChecker;
 
-
 /**
  * Solution to problem 27:
  * 
@@ -11,10 +10,11 @@ import utilities.PrimalityChecker;
  * 		n² + n + 41
  * 
  * It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39.
- * However, when n = 40, 40^2 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
+ * However, when n = 40, 40^2 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and
+ * certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
  * 
- * The incredible formula  n² - 79n + 1601 was discovered, which produces 80 primes for the consecutive values n = 0 to 79.
- * The product of the coefficients, -79 and 1601, is -126479.
+ * The incredible formula  n² - 79n + 1601 was discovered, which produces 80 primes for
+ * the consecutive values n = 0 to 79. The product of the coefficients, -79 and 1601, is -126479.
  * 
  * Considering quadratics of the form:
  * 
@@ -22,7 +22,8 @@ import utilities.PrimalityChecker;
  * 
  * 		where |n| is the modulus/absolute value of n
  * 		e.g. |11| = 11 and |-4| = 4
- * Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n = 0.
+ * Find the product of the coefficients, a and b, for the quadratic expression that produces the
+ * maximum number of primes for consecutive values of n, starting with n = 0.
  */
 public class QuadraticPrimes {
 
