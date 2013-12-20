79129bc9551a2191e59258287e4dd7fabaaf2f09
Daniel Temple <DanielTempleUK@GoogleMail.com>
2013-12-20 09:52:07 +0000

    Committing working solution to problem 50

--- src/problem50/ConsecutivePrimeSum.java
+++ src/problem50/ConsecutivePrimeSum.java
@@ -6,6 +6,19 @@ import java.util.List;
 
 import utilities.PrimalityChecker;
 
+/**
+ * Solution to problem 50:
+ * 
+ * 
+ * The prime 41, can be written as the sum of six consecutive primes:
+ *     41 = 2 + 3 + 5 + 7 + 11 + 13
+ * This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+ * 
+ * The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+ * Which prime, below one-million, can be written as the sum of the most consecutive primes?
+ * 
+ * NOTE: This could do with some optimisation. It takes around 40 seconds to run.
+ */
 public class ConsecutivePrimeSum {
 
 	public static void main(final String[] args) throws IOException {
@@ -19,14 +32,30 @@ public class ConsecutivePrimeSum {
 
 	private static long calculateSolution() {
 		final List<Long> primes = getPrimes();
+		System.out.println(primes.size());
 
 		long longestSequence = 0L;
 		long primeWithSequence = 0L;
 
-		for(int i = 167; i<primes.size(); i++) {
-
+		for (int i = 0; i < primes.size(); i++) {
 			long total = 0L;
 			long sequence = 0L;
+			for(int j = 0; j <= i; j++) {
+				total = total + primes.get(j);
+				sequence++;
+
+				if(total > 1000000){
+					break;
+				}
+
+				if( PrimalityChecker.isPrime(total) && sequence > longestSequence) {
+					longestSequence = sequence;
+					primeWithSequence = total;
+				}
+			}
+
+			total = 0L;
+			sequence = 0L;
 			for(int j = i; j < primes.size(); j++) {
 				total = total + primes.get(j);
 				sequence++;
@@ -35,13 +64,11 @@ public class ConsecutivePrimeSum {
 					break;
 				}
 
-				if( primes.contains(total) && sequence > longestSequence) {
+				if( PrimalityChecker.isPrime(total) && sequence > longestSequence) {
 					longestSequence = sequence;
 					primeWithSequence = total;
 				}
-
 			}
-
 		}
 
 		System.out.println("Sequence is: " + longestSequence);
